efbf0cd182deac862df4954335691e1db969cbb7
Juanco <juanffo874@gmail.com>
2024-11-27 18:48:30 -0600

    Crea las vistas de autenticacion y las añade a las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react"
 import { Route, Routes } from "react-router"
 import Home from "./views/Home"
+import Login from "./views/auth/Login"
+import Register from "./views/auth/Register"
+import ForgotPass from "./views/auth/ForgotPass"
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route index path="/" element={<Home/>}/>
+        <Route path="/auth/login" element={<Login/>}/>
+        <Route path="/auth/register" element={<Register/>}/>
+        <Route path="/auth/forgot-password" element={<ForgotPass/>}/>
       </Routes>
     </>
   )
