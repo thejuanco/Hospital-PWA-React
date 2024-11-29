decd2927dc86eccc6ad033db3f874dcd1d9c71c7
Juanco <juanffo874@gmail.com>
2024-11-29 08:51:29 -0600

    Crea el componente para los servicios del modulo e instala materia symbols para los iconos

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Navbar from '../components/auth/Navbar'
+import Services from '../components/home/Services';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <>
+      <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+        <Navbar />
+        <div className="space-y-20">
+          <div className="mt-10">
+            <h1 className="font-bold text-5xl text-center">
+              Radiogia e Imagen
+            </h1>
+            <p className="mt-3 font-semibold text-center">
+              Diagnosticos precisos con tecnologia de vanguardia
+            </p>
+          </div>
+          <Services />
+          </div>
+        </div>
+      </>
+  );
 }
 
 export default Home
