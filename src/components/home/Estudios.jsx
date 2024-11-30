828f2874999394c9ecb397f012deffc54705bffd
Juanco <juanffo874@gmail.com>
2024-11-29 20:30:28 -0600

    Crea el componente para conocer los registros

---
+++ src/components/home/Estudios.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Estudios = () => {
+  return (
+    <>
+        <div className='p-4 rounded-lg'>
+            <h1>Estudios</h1>
+            <p>Aquí podrás encontrar información sobre los estudios que hemos realizado en el hospital.</p>
+            <button className='bg-purple-500 text-white rounded-lg px-4 py-2 font-bold'>Ver más</button>
+        </div>
+    </>
+  )
+}
+
+export default Estudios
