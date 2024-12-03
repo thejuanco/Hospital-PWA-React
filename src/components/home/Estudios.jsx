ac006168b79ea0e50222d764c2fab609232fc975
Juanco <210373@utxicotepec.edu.mx>
2024-12-03 14:26:11 -0600

    Obtiene los datos de los estudios y los renderiza

--- src/components/home/Estudios.jsx
+++ src/components/home/Estudios.jsx
@@ -3,6 +3,7 @@ import useAxios from '../../api/api.auth';
 import Sidebar from './Sidebar'
 
 const Estudios = () => {
+  const [estudios, setEstudios] = useState([])
 
   const intanceAPI = useAxios();
 
@@ -10,7 +11,7 @@ const Estudios = () => {
     const getStudios = async () => {
       try {
         const result = await intanceAPI.get('/estudios')
-        console.log(result.data)
+        setEstudios(result.data)
       } catch (error) {
         console.log(error)
       }
@@ -23,7 +24,23 @@ const Estudios = () => {
       <Sidebar />
         <div className='p-4 sm:ml-64'>
             <div>
-              Desde estudios
+              <div className='flex justify-between'>
+                <h1 className='text-3xl font-bold'>Estudios</h1>
+                <button className='bg-gray-900 rounded-full px-4 font-semibold text-center text-gray-100 mr-10'>
+                  Nuevo Estudio
+                </button>
+              </div>
+
+              <div className='grid grid-cols-2 gap-4 mt-10 '>
+                {estudios.map(estudio => (
+                  <div key={estudio.id} className='border p-2 rounded-lg hover:bg-gray-100'>
+                    <p>{estudio.Tipo}</p>
+                    <p>{estudio.Nivel_Urgencia}</p>
+                    <p>{estudio.Observaciones}</p>
+                    <p>{estudio.Dirigido_A}</p>
+                  </div>
+                ))}
+              </div>
             </div>
         </div>
     </>
