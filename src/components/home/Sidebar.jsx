c8736757b32ed84b0fc6ffe0a8822ca3df822f65
Juanco <210373@utxicotepec.edu.mx>
2024-12-03 13:54:08 -0600

    Modifica los estilos al dashboard

--- src/components/home/Sidebar.jsx
+++ src/components/home/Sidebar.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router";
 const Sidebar = () => {
   return (
     <>
-      <div className='class="flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800'>
-        <div className='class="fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r'>
+      <div className='flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800'>
+        <div className='fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r'>
           <div className="overflow-y-auto overflow-x-hidden flex-grow">
             <ul className="flex flex-col py-4 space-y-1">
               <li>
