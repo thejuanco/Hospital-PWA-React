b3898f3e2932b9455c95ba9fc4d0537112b8ed87
Juanco <juanffo874@gmail.com>
2024-12-03 19:45:30 -0600

    Modificando los iconos para el sidebar

--- src/components/home/Sidebar.jsx
+++ src/components/home/Sidebar.jsx
@@ -28,7 +28,7 @@ const Sidebar = () => {
                   className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6"
                 >
                   <span className="inline-flex justify-center items-center ml-4">
-                    <span className="material-symbols-rounded">task</span>
+                    <span className="material-symbols-rounded">monitor_heart</span>
                   </span>
                   <span className="ml-2 text-sm tracking-wide truncate">
                     Estudios
@@ -42,7 +42,7 @@ const Sidebar = () => {
                   className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6"
                 >
                   <span className="inline-flex justify-center items-center ml-4">
-                    <span className="material-symbols-rounded">workspaces</span>
+                    <span className="material-symbols-rounded">biotech</span>
                   </span>
                   <span className="ml-2 text-sm tracking-wide truncate">
                     Resultados Estudios
