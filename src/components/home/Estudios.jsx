cf990073c8cd0b9d63a6dd04125e45da5ccb5220
Juanco <juanffo874@gmail.com>
2024-12-03 19:12:36 -0600

    Instala un indicador de carga y lo agrega a la vista

--- src/components/home/Estudios.jsx
+++ src/components/home/Estudios.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import useAxios from '../../api/api.auth';
 import Sidebar from './Sidebar'
+import { ThreeDots } from 'react-loader-spinner';
 
 const Estudios = () => {
   const [estudios, setEstudios] = useState([])
@@ -31,15 +32,35 @@ const Estudios = () => {
                 </button>
               </div>
 
-              <div className='grid grid-cols-2 gap-4 mt-10 '>
-                {estudios.map(estudio => (
-                  <div key={estudio.id} className='border p-2 rounded-lg hover:bg-gray-100'>
-                    <p>{estudio.Tipo}</p>
-                    <p>{estudio.Nivel_Urgencia}</p>
-                    <p>{estudio.Observaciones}</p>
-                    <p>{estudio.Dirigido_A}</p>
+              <div className='grid sm:grid-cols-2 md:grid-cols-2 gap-4 mt-10 '>
+                {estudios.length == 0 ? (
+                  <div className='flex justify-center items-center mt-12 text-center'>
+                    <p className='text-xl font-bold'>Aún no hay estudios</p>
+                    <ThreeDots
+                      visible={true}
+                      height="70"
+                      width="70"
+                      color="#000000"
+                      radius="9"
+                      ariaLabel="three-dots-loading"
+                      wrapperStyle={{}}
+                      wrapperClass="mx-6"
+                    />
                   </div>
-                ))}
+                ) : (
+                  estudios.map(estudio => (
+                    <div key={estudio.id} className='border p-2 rounded-lg hover:bg-gray-100'>
+                      <p className='font-semibold text-lg'>{estudio.Tipo}</p>
+                      <p>{estudio.Observaciones}</p>
+                      <p>{estudio.Nivel_Urgencia}</p>
+                      <p>{estudio.Dirigido_A}</p>
+                      <div className='flex justify-between mx-10 py-1 mt-2'>
+                        <button className='bg-gray-900 text-white font-semibold px-10 rounded-full'>Editar</button>
+                        <button className='border font-semibold px-10 rounded-full'>Eliminar</button>
+                      </div>
+                    </div>
+                  ))
+                )}
               </div>
             </div>
         </div>
