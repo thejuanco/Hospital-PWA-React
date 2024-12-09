8e6fe65ec0b158bbb29e00369d27fd764be21589
Juanco <juanffo874@gmail.com>
2024-12-09 16:17:13 -0600

    Renombra class como className

--- src/components/home/Sidebar.jsx
+++ src/components/home/Sidebar.jsx
@@ -16,7 +16,7 @@ const Sidebar = () => {
                   to="/dashboard"
                   className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6"
                 >
-                  <span class="inline-flex justify-center items-center ml-4">
+                  <span className="inline-flex justify-center items-center ml-4">
                     <span className="material-symbols-rounded">home</span>
                   </span>
                   <span className="ml-2 text-sm tracking-wide truncate">
