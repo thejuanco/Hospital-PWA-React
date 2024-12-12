0aad993d38d125fe30e25f2b5ae431e508952d07
Juanco <juanffo874@gmail.com>
2024-12-12 11:06:49 -0600

    Agrega resposividad para el home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -31,56 +31,61 @@ const Home = () => {
       <div className="p-6">
         <div className="bg-white shadow-md rounded-lg overflow-hidden">
           <h2 className="font-semibold text-xl p-4">Estudios Pendientes</h2>
-          <table className="min-w-full table-auto">
-            <thead>
-              <tr className="bg-gray-200 text-gray-600 text-sm leading-normal">
-                <th className="py-3 px-6 text-left">Tipo</th>
-                <th className="py-3 px-6 text-left">Nivel de Urgencia</th>
-                <th className="py-3 px-6 text-left">Estatus</th>
-                <th className="py-3 px-6 text-left">Costo Total</th>
-                <th className="py-3 px-6 text-left">Dirigido A</th>
-                <th className="py-3 px-6 text-left">Fecha de Registro</th>
-              </tr>
-            </thead>
-            <tbody className="text-gray-600 text-sm font-light">
-              {studios.length > 0 ? (
-                studios.map((studio) => (
-                  <tr
-                    key={studio.id}
-                    className="border-b border-gray-200 hover:bg-gray-100"
-                  >
-                    <td className="py-3 px-6 text-left whitespace-nowrap">
-                      {studio.Tipo}
-                    </td>
-                    <td className="py-3 px-6 text-left">
-                      {studio.Nivel_Urgencia}
-                    </td>
-                    <td className="py-3 px-6 text-left">{studio.Estatus}</td>
-                    <td className="py-3 px-6 text-left">
-                      ${studio.Total_Costo}
-                    </td>
-                    <td className="py-3 px-6 text-left">{studio.Dirigido_A}</td>
-                    <td className="py-3 px-6 text-left">
-                      {new Date(studio.Fecha_Registro).toLocaleDateString()}
+          <div className="overflow-x-auto">
+            <table className="min-w-full table-auto">
+              <thead>
+                <tr className="bg-gray-200 text-gray-600 text-sm leading-normal">
+                  <th className="py-3 px-6 text-left">Tipo</th>
+                  <th className="py-3 px-6 text-left">Nivel de Urgencia</th>
+                  <th className="py-3 px-6 text-left">Estatus</th>
+                  <th className="py-3 px-6 text-left">Costo Total</th>
+                  <th className="py-3 px-6 text-left">Dirigido A</th>
+                  <th className="py-3 px-6 text-left">Fecha de Registro</th>
+                </tr>
+              </thead>
+              <tbody className="text-gray-600 text-sm font-light">
+                {studios.length > 0 ? (
+                  studios.map((studio) => (
+                    <tr
+                      key={studio.id}
+                      className="border-b border-gray-200 hover:bg-gray-100"
+                    >
+                      <td className="py-3 px-6 text-left whitespace-nowrap">
+                        {studio.Tipo}
+                      </td>
+                      <td className="py-3 px-6 text-left">
+                        {studio.Nivel_Urgencia}
+                      </td>
+                      <td className="py-3 px-6 text-left">{studio.Estatus}</td>
+                      <td className="py-3 px-6 text-left">
+                        ${studio.Total_Costo}
+                      </td>
+                      <td className="py-3 px-6 text-left">
+                        {studio.Dirigido_A}
+                      </td>
+                      <td className="py-3 px-6 text-left">
+                        {new Date(studio.Fecha_Registro).toLocaleDateString()}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan="6" className="text-center py-4">
+                      No hay estudios pendientes.
                     </td>
                   </tr>
-                ))
-              ) : (
-                <tr>
-                  <td colSpan="6" className="text-center py-4">
-                    No hay estudios pendientes.
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-      <div className="flex flex-row justify-between items-center mt-6 gap-4">
-        <div className="flex-1 min-w-[300px]">
+
+      <div className="flex flex-col md:flex-row justify-between items-center mt-6 gap-4">
+        <div className="flex-1 min-w-[300px] w-full">
           <UrgencyChart studios={studios} />
         </div>
-        <div className="flex-1 min-w-[400px]">
+        <div className="flex-1 min-w-[400px] w-full">
           <StudyStatusChart />
         </div>
       </div>
